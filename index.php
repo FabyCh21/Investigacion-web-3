14448f951a871f46af1a8efd74e67e92201ac7d7
FabyCh21 <fabychaves@live.com>
2017-05-15 10:50:20 -0600

    FINAL

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully";
 
+mysqli_select_db($conn,"webpao");
+
 
 echo ('<h1>Web Scrapping Paola</h1>');
             $file = file_get_contents('http://es.mobafire.com/league-of-legends/campeones');
@@ -39,6 +41,28 @@ echo ('<h1>Web Scrapping Paola</h1>');
                 $nombres[$j]= str_replace(".", '', $nombres[$j]);
             }
 
+            $sql = "INSERT INTO auditoria (fecha, pagina_web, estado)
+                        VALUES (NOW(), 
+                            'http://gameinfo.lan.leagueoflegends.com/es/game-info/champions/', 
+                            'pendiente')";
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+            $sql = "SELECT codigo FROM auditoria ORDER BY codigo DESC LIMIT 1";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo $row["codigo"];
+                    $AUDIOTORIA_COD = $row["codigo"];
+                }
+            } else {
+                echo "0 results";
+            }
+
             for($j = 0; $j < 40; $j++){
                 $file = file_get_contents('http://gameinfo.lan.leagueoflegends.com/es/game-info/champions/'.$nombres[$j].'/');
                 libxml_use_internal_errors(true);
@@ -46,7 +70,10 @@ echo ('<h1>Web Scrapping Paola</h1>');
                 $doc->loadHTML($file);
                 libxml_clear_errors(); 
 
+                
+
                 $xpath = new DOMXPath($doc);
+
 
                 $name = $xpath->query("//div[@class='default-2-3']/h3");
                 $CHAMP_name = $name[0]->nodeValue;
@@ -105,6 +132,42 @@ echo ('<h1>Web Scrapping Paola</h1>');
                 echo ('<img src="'.$src2.'">');
                 echo "</br>";
 
+                $sql = "INSERT INTO registros  (Cod_Auditoria, nomre, icon_URL, vida, danno, movimiento, regeneracion, 
+                    armadura, rest_magica, descripcion, url_ab_1, url_ab_2, url_ab_3, url_ab_4, url_ab_5)
+                        VALUES (".$AUDIOTORIA_COD.",
+                                '".$CHAMP_name."',
+                                '".$CHAMP_Icon."',
+                                '".$CHAMP_vida."',
+                                '".$CHAMP_ataque."',
+                                '".$CHAMP_movmiento."',
+                                '".$CHAMP_regVida."',
+                                '".$CHAMP_armadura."',
+                                '".$CHAMP_restMagica."',
+                                '".$CHAMP_desc."',
+                                '".$CHAMP_AB1."',
+                                '".$CHAMP_AB2."',
+                                '".$CHAMP_AB3."',
+                                '".$CHAMP_AB4."',
+                                '".$CHAMP_AB5."'
+                                )";
+                if ($conn->query($sql) === TRUE) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+
+
+                $sql = "SELECT codigo FROM registros ORDER BY codigo DESC LIMIT 1";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo $row["codigo"];
+                        $REG_COD = $row["codigo"];
+                    }
+                } else {
+                    echo "0 results";
+                }
 
                 echo "<p>Aspectos:</p>";
                 $skinsIMG = $xpath->query("//a[contains(@class,'skins')]/img");
@@ -113,6 +176,16 @@ echo ('<h1>Web Scrapping Paola</h1>');
                 for ($i = 0; $i < $skinsIMG->length; $i++){
                     $srcSkin = $skinsIMG[$i]->getAttribute('src');
                     echo ('<img src="'.$srcSkin.'" width="'.$size.'%">');
+
+
+                $sql = "INSERT INTO aspecto  (champ, url_Aspecto)
+                        VALUES (".$REG_COD.",
+                                '".$srcSkin."')";
+                if ($conn->query($sql) === TRUE) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
                 }
 
                 echo "<br>";
